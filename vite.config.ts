8c46825e519d60b288534e54349c7e7a59a7d4ee
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-05-30 18:05:39 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test_logic-like/'
+  base: 'https://johnnystorm19.github.io/test_logic-like/'
 })
